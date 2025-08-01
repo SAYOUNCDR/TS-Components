ceff4d7819e13bcde09aa71b65f801eea160f453
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-08-01 18:34:40 +0530

    React + TypeScript + TailwindCSS v4

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import ClipboardCopy from "./components/ClipboardCopy"
 
 const App = () => {
   return (
-    <div>
-      <h1>APP File</h1>
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-2xl font-bold text-center p-20">APP File</h1>
       <ClipboardCopy text="Sample text for copy!"/>
     </div>
   )
